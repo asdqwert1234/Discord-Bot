72da03c1aa49b57bf8c14ccedb53f8e040741bd6
Victor9bio <pooh9527@gmail.com>
2024-08-19 01:46:22 +0800

    Update time_parameters.py

--- data/time_parameters.py
+++ data/time_parameters.py
@@ -3,4 +3,4 @@ import datetime
 # 時區設置 'Asia/Taipei'
 tz = datetime.timezone(datetime.timedelta(hours = 8))
 # 每日凌晨12點
-DAILY_CLOCK = datetime.time(hour = 1, minute = 45, second = 0, tzinfo= tz)
+DAILY_CLOCK = datetime.time(hour = 1, minute = 49, second = 0, tzinfo= tz)
