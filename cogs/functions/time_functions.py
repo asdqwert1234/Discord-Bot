db18a93844f8e7a2d527354a7164f8b784c33b97
Victor9bio <pooh9527@gmail.com>
2024-08-19 01:54:38 +0800

    Update time_functions.py

--- cogs/functions/time_functions.py
+++ cogs/functions/time_functions.py
@@ -4,5 +4,5 @@ from data.time_parameters import DAILY_CLOCK, tz
 # 確認現在時間是否相同 
 def check_times_up() -> bool :
     now = datetime.datetime.now(tz)
-    print(now)
+    #print(now)
     return True if (now.hour == DAILY_CLOCK.hour) and (now.minute == DAILY_CLOCK.minute) else False
